1fbc7a49a54d9bf7854d3d29e11be9398004b2ec
milimolo <bondemand@hotmail.dk>
2023-04-17 10:22:03 +0200

    Made Ticket model

--- Ticket-System-Backend/TicketApi/Models/Ticket.cs
+++ Ticket-System-Backend/TicketApi/Models/Ticket.cs
@@ -2,5 +2,11 @@
 {
     public class Ticket
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Location { get; set; }
+        public DateTime EventDate { get; set; }
+        public decimal Price { get; set; }
+        public int TicketsRemaining { get; set; }
     }
 }
