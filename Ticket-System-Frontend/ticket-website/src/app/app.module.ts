12e1ade0b82cbbe1f7d20e45ba9d91685537d215
milimolo <bondemand@hotmail.dk>
2023-05-04 14:57:30 +0200

    Added Css from a template aswell as images in assets folder

--- Ticket-System-Frontend/ticket-website/src/app/app.module.ts
+++ Ticket-System-Frontend/ticket-website/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { TicketComponent } from './tickets/ticket/ticket.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { LoginComponent } from './shared/login/login.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { HomeComponent } from './home/home/home.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
     TicketsComponent,
     TicketComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
